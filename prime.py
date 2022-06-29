8c259a699220a81c4d5486a9f33fc2300a0c65a5
Maulccodes <41537069+Maulccodes@users.noreply.github.com>
2022-06-29 09:28:39 -0400

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,10 @@
+num = 13
+if num &gt; 1:
+    fori in range(2,num//2):
+        if (num%i)==0:
+            print(num, "is not a prime number")
+            break
+            else:
+            print(num, "is a prime number")
+            else:
+            print(num,"is not a prime number")
