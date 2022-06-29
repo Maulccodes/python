f1d4bab7ab57bd8742d08b09f9f1b5569ca80727
Maulccodes <41537069+Maulccodes@users.noreply.github.com>
2022-06-29 09:01:00 -0400

    Add files via upload

---
+++ Natural_Numbers.py
@@ -0,0 +1,15 @@
+from ast import Num
+import numbers
+
+
+num = int(input("Enter a number:"))
+
+if num < 0:
+    print("Enter a positive number")
+Else:
+    sum = 0
+    # use while lopp to iterate until zero
+    while(num>o):
+        sum += num
+        num -=1
+        print("the sum is",sum)
